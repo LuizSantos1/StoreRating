860a61bc307423bb2be426d7839b4d8c591c1f28
MauMau <kistters.code@gmail.com>
2018-07-12 18:09:49 -0300

    sumary query stars

--- app/code/community/Goat/StoreRating/Model/Resource/Rating/Collection.php
+++ app/code/community/Goat/StoreRating/Model/Resource/Rating/Collection.php
@@ -13,4 +13,18 @@ class Goat_StoreRating_Model_Resource_Rating_Collection extends Mage_Core_Model_
     {
         $this->_init('storerating/rating');
     }
+
+    /**
+     *
+     *  perform summary stars qty
+     */
+    public function getStarsQtySummary()
+    {
+		$this->getSelect()
+			->reset(Zend_Db_Select::COLUMNS)
+			->columns('stars AS star, COUNT(*) AS qty')
+			->group('main_table.stars');
+		
+		return $this;
+    }
 }
