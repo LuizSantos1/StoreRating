af51dfe58a67995601e3563395c17a269afd12db
MauMau <kistters.code@gmail.com>
2018-07-13 15:42:22 -0300

    set limit and pagination

--- app/code/community/Goat/StoreRating/Block/Rating/List.php
+++ app/code/community/Goat/StoreRating/Block/Rating/List.php
@@ -30,6 +30,8 @@ class Goat_StoreRating_Block_Rating_List extends Mage_Core_Block_Template
 
             $this->_storeRatingCollection->setOrder('stars', 'DESC');
             $this->_storeRatingCollection->setOrder('created_at', 'DESC');
+            
+            #
         }
         return $this->_storeRatingCollection;
     }
@@ -42,5 +44,53 @@ class Goat_StoreRating_Block_Rating_List extends Mage_Core_Block_Template
     public function getLoadedStoreRatingCollection()
     {
         return $this->_getStoreRatingCollection();
+    }
+
+    /**
+     * Need use as _prepareLayout - but problem in declaring collection from
+     * another block (was problem with search result)
+     */
+    protected function _beforeToHtml()
+    {
+        // called prepare sortable parameters
+        $collection = $this->_getStoreRatingCollection();
+
+        $curPage = (int) $this->getRequest()->getParam('p', 1);
+        $pageSize = (int) $this->getRequest()->getParam('limit', 20);
+        if ($pageSize > 40) {
+            $pageSize = 40;
+        }
+        $collection->setPageSize($pageSize)->setCurPage($curPage);
+
+        $this->_getStoreRatingCollection()->load();
+
+        return parent::_beforeToHtml();
+    }
+
+    public function getPagerUrl($params=array())
+    {
+        $urlParams = array();
+        $urlParams['_current']  = true;
+        $urlParams['_escape']   = true;
+        $urlParams['_use_rewrite']   = true;
+        $urlParams['_query']    = $params;
+        return $this->getUrl('*/*/*', $urlParams);
+    }
+
+    public function getPageUrl($page)
+    {
+        return $this->getPagerUrl(array('p' => $page));
+    }
+
+    /**
+     * @return int $totalPages
+     */
+    public function getTotalPages()
+    {
+        $collection = $this->_getStoreRatingCollection();
+
+        $totalPages = ceil($collection->getSize() / $collection->getPageSize());
+
+        return $totalPages;
     }
 }
